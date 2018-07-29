bd89a1f19ce922bc95953090442bc2cd2f93f07b
suraj pagare <surajmoagare75@gmail.com>
2018-07-29 15:02:47 +0530

    second commit itemclass added

---
+++ app/src/main/java/com/example/surajpagare/googlelocation/Item.java
@@ -0,0 +1,28 @@
+package com.example.surajpagare.googlelocation;
+
+/**
+ * Created by suraj pagare on 7/29/2018.
+ */
+
+public class Item {
+
+    String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name;
+}
